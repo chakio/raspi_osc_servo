cc5d5fa6db59b2e651cf2d6aa7f946ca0f9510af
HATTORI Kohei <hattori@team-lab.com>
2016-06-15 20:54:12 +0900

    first commit

--- src/oscListener.cpp
+++ src/oscListener.cpp
@@ -66,58 +66,18 @@ void raspiTest::ProcessMessage( const osc::ReceivedMessage& m,
     try{
         if( strcmp( m.AddressPattern(), "/deviceA" ) == 0 ){
             osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-            osc::int32 val0, val1, val2, val3, val4, val5,
-                    val6, val7, val8, val9, val10, val11,
-                    val12, val13, val14, val15, val16, val17,
-                    val18, val19, val20, val21, val22, val23;
 
-            args >> val0 >> val1 >> val2 >> val3 >> val4 >> val5
-                    >> val6 >> val7 >> val8 >> val9 >> val10 >> val11
-                    >> val12 >> val13 >> val14 >> val15 >> val16 >> val17
-                    >> val18 >> val19 >> val20 >> val21 >> val22 >> val23
+            args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
+                    >> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
+                    >> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
+                    >> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
+                    >> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
+                    >> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
                     >> osc::EndMessage;
 
-            values[0] = (int)val0;
-            values[1] = (int)val1;
-            values[2] = (int)val2;
-            values[3] = (int)val3;
-            values[4] = (int)val4;
-            values[5] = (int)val5;
-            values[6] = (int)val6;
-            values[7] = (int)val7;
-            values[8] = (int)val8;
-            values[9] = (int)val9;
-            values[10] = (int)val10;
-            values[11] = (int)val11;
-            values[12] = (int)val12;
-            values[13] = (int)val13;
-            values[14] = (int)val14;
-            values[15] = (int)val15;
-            values[16] = (int)val16;
-            values[17] = (int)val17;
-            values[18] = (int)val18;
-            values[19] = (int)val19;
-            values[20] = (int)val20;
-            values[21] = (int)val21;
-            values[22] = (int)val22;
-            values[23] = (int)val23;
-
-
-            std::cout << "received '/deviceA' message with arguments: "
-                      << val0 << " " << val1 << " " << val2 << " " << val3 << " "
-                      << val4 << " " << val5 << " " << val6 << " " << val7 << " "
-                      << val8 << " " << val9 << " " << val10 << " " << val11 << " "
-                      << val12 << " " << val13 << " " << val14 << " " << val15 << " "
-                      << val16 << " " << val17 << " " << val18 << " " << val19 << " "
-                      << val20 << " " << val21 << " " << val22 << " " << val23 << " "
-                      << "\n";
-
-            for(int i=0; i<1; i++) {
-                command = dioList[i].getPWMPalseChangeCommand(values);
-                dioList[i].sendCommand(command);
-                //valList.clear();
-            }
-
+            for(int j=0; j<PWM_CHANNEL_NUM; j++) values[j] = (int)oscValues[j];
+            command = dioList[0].getPWMPalseChangeCommand(values);
+            dioList[0].sendCommand(command);
 
         } else if( strcmp( m.AddressPattern(), "/deviceB" ) == 0 ){
             osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
