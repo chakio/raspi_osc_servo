8a4be5e9799c718d371b32abbbc3a9357ef2aad0
HATTORI Kohei <hattori@team-lab.com>
2016-06-15 21:14:36 +0900

    first commit

--- src/oscListener.cpp
+++ src/oscListener.cpp
@@ -4,34 +4,34 @@
 void raspiTest::dioStart() {
 
     // init dio
-    for(int i=0; i<1; i++){
+    for(int i=0; i<DIO_NUM; i++){
         Dacs1500rcp24 dio(i);
         dioList.push_back(dio);
     }
 
     // device open
-    for(int i=0; i<1; i++) dioList[i].open();
+    for(int i=0; i<DIO_NUM; i++) dioList[i].open();
 
     // lamp on -> off
-    for(int i=0; i<1; i++) {
+    for(int i=0; i<DIO_NUM; i++) {
         command = dioList[i].getDigitalOutPutCommand("800000");
       dioList[i].sendCommand(command);
     }
     sleep(2);
-    for(int i=0; i<1; i++) {
+    for(int i=0; i<DIO_NUM; i++) {
         command = dioList[i].getDigitalOutPutCommand("000000");
         dioList[i].sendCommand(command);
     }
     sleep(2);
 
     // pwm init
-    for(int i=0; i<1; i++) {
+    for(int i=0; i<DIO_NUM; i++) {
         command = dioList[i].getPWMInitializeCommand(3,20000);
         dioList[i].sendCommand(command);
     }
 
     // pwm init pos
-    for(int i=0; i<1; i++) {
+    for(int i=0; i<DIO_NUM; i++) {
         for(int j=0; j<PWM_CHANNEL_NUM; j++) {
             oscValues.push_back(1450);
             values.push_back(1450);
@@ -41,7 +41,7 @@ void raspiTest::dioStart() {
     }
 
     // start
-    for(int i=0; i<1; i++) {
+    for(int i=0; i<DIO_NUM; i++) {
         command = dioList[i].getPWMStartCommand();
         dioList[i].sendCommand(command);
     }
@@ -50,13 +50,13 @@ void raspiTest::dioStart() {
 
 void raspiTest::dioStop() {
     // device pwm stop
-    for(int i=0; i<1; i++) {
+    for(int i=0; i<DIO_NUM; i++) {
         command = dioList[i].getPWMStopCommand();
         dioList[i].sendCommand(command);
     }
 
     // device close
-    for(int i=0; i<1; i++) dioList[i].close();
+    for(int i=0; i<DIO_NUM; i++) dioList[i].close();
 }
 
 
@@ -65,70 +65,67 @@ void raspiTest::ProcessMessage( const osc::ReceivedMessage& m,
 {
     try{
         if( strcmp( m.AddressPattern(), "/deviceA" ) == 0 ){
-            osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-
-            args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
-                    >> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
-                    >> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
-                    >> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
-                    >> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
-                    >> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
-                    >> osc::EndMessage;
-
-            for(int j=0; j<PWM_CHANNEL_NUM; j++) values[j] = (int)oscValues[j];
-            command = dioList[0].getPWMPalseChangeCommand(values);
-            dioList[0].sendCommand(command);
+            if(DIO_NUM >= 1) {
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+                args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
+                        >> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
+                        >> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
+                        >> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
+                        >> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
+                        >> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
+                        >> osc::EndMessage;
+                for(int j=0; j<PWM_CHANNEL_NUM; j++) values[j] = (int)oscValues[j];
+                command = dioList[0].getPWMPalseChangeCommand(values);
+                dioList[0].sendCommand(command);
+            }
 
         } else if( strcmp( m.AddressPattern(), "/deviceB" ) == 0 ){
-            osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-
-            args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
-                    >> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
-                    >> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
-                    >> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
-                    >> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
-                    >> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
-                    >> osc::EndMessage;
-
-            for(int i=0; i<1; i++) {
+            if(DIO_NUM >= 2) {
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+                args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
+                        >> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
+                        >> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
+                        >> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
+                        >> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
+                        >> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
+                        >> osc::EndMessage;
                 for(int j=0; j<PWM_CHANNEL_NUM; j++) values[j] = (int)oscValues[j];
-                command = dioList[i].getPWMPalseChangeCommand(values);
-                dioList[i].sendCommand(command);
+                command = dioList[1].getPWMPalseChangeCommand(values);
+                dioList[1].sendCommand(command);
             }
 
         } else if( strcmp( m.AddressPattern(), "/deviceC" ) == 0 ){
-            osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-
-            args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
-                    >> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
-                    >> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
-                    >> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
-                    >> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
-                    >> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
-                    >> osc::EndMessage;
-
-            for(int i=0; i<1; i++) {
+            if(DIO_NUM >= 3) {
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+                args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
+                        >> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
+                        >> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
+                        >> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
+                        >> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
+                        >> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
+                        >> osc::EndMessage;
                 for(int j=0; j<PWM_CHANNEL_NUM; j++) values[j] = (int)oscValues[j];
-                command = dioList[i].getPWMPalseChangeCommand(values);
-                dioList[i].sendCommand(command);
+                command = dioList[2].getPWMPalseChangeCommand(values);
+                dioList[2].sendCommand(command);
             }
 
         }  else if( strcmp( m.AddressPattern(), "/deviceD" ) == 0 ){
-            osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+            if(DIO_NUM >= 4) {
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
 
-            args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
-                    >> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
-                    >> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
-                    >> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
-                    >> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
-                    >> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
-                    >> osc::EndMessage;
+                args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
+                        >> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
+                        >> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
+                        >> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
+                        >> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
+                        >> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
+                        >> osc::EndMessage;
 
-            for(int i=0; i<1; i++) {
                 for(int j=0; j<PWM_CHANNEL_NUM; j++) values[j] = (int)oscValues[j];
-                command = dioList[i].getPWMPalseChangeCommand(values);
-                dioList[i].sendCommand(command);
+                command = dioList[3].getPWMPalseChangeCommand(values);
+                dioList[3].sendCommand(command);
             }
+
         }
     } catch( osc::Exception& e ) {
         std::cout << "error while parsing message: "
