dd9d73e9ebdc25506528b3b892484842cd993017
HATTORI Kohei <hattori@team-lab.com>
2016-06-15 16:36:21 +0900

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -12,98 +12,98 @@ class ExamplePacketListener : public osc::OscPacketListener {
 protected:
 
     virtual void ProcessMessage( const osc::ReceivedMessage& m,
-				const IpEndpointName& remoteEndpoint )
+                                 const IpEndpointName& remoteEndpoint )
     {
         try{
             if( strcmp( m.AddressPattern(), "/deviceA" ) == 0 ){
                 osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
                 osc::int32 val0, val1, val2, val3, val4, val5,
-                           val6, val7, val8, val9, val10, val11,
-                           val12, val13, val14, val15, val16, val17,
-                           val18, val19, val20, val21, val22, val23;
+                        val6, val7, val8, val9, val10, val11,
+                        val12, val13, val14, val15, val16, val17,
+                        val18, val19, val20, val21, val22, val23;
 
                 args >> val0 >> val1 >> val2 >> val3 >> val4 >> val5
-                     >> val6 >> val7 >> val8 >> val9 >> val10 >> val11
-                     >> val12 >> val13 >> val14 >> val15 >> val16 >> val17
-                     >> val18 >> val19 >> val20 >> val21 >> val22 >> val23
-                     >> osc::EndMessage;
+                        >> val6 >> val7 >> val8 >> val9 >> val10 >> val11
+                        >> val12 >> val13 >> val14 >> val15 >> val16 >> val17
+                        >> val18 >> val19 >> val20 >> val21 >> val22 >> val23
+                        >> osc::EndMessage;
                 std::cout << "received '/deviceA' message with arguments: "
-                    << val0 << " " << val1 << " " << val2 << " " << val3 << " "
-                    << val4 << " " << val5 << " " << val6 << " " << val7 << " "
-                    << val8 << " " << val9 << " " << val10 << " " << val11 << " "
-                    << val12 << " " << val13 << " " << val14 << " " << val15 << " "
-                    << val16 << " " << val17 << " " << val18 << " " << val19 << " "
-                    << val20 << " " << val21 << " " << val22 << " " << val23 << " "
-                    << "\n";
+                          << val0 << " " << val1 << " " << val2 << " " << val3 << " "
+                          << val4 << " " << val5 << " " << val6 << " " << val7 << " "
+                          << val8 << " " << val9 << " " << val10 << " " << val11 << " "
+                          << val12 << " " << val13 << " " << val14 << " " << val15 << " "
+                          << val16 << " " << val17 << " " << val18 << " " << val19 << " "
+                          << val20 << " " << val21 << " " << val22 << " " << val23 << " "
+                          << "\n";
 
             } else if( strcmp( m.AddressPattern(), "/deviceB" ) == 0 ){
-              osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-              osc::int32 val0, val1, val2, val3, val4, val5,
-                         val6, val7, val8, val9, val10, val11,
-                         val12, val13, val14, val15, val16, val17,
-                         val18, val19, val20, val21, val22, val23;
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+                osc::int32 val0, val1, val2, val3, val4, val5,
+                        val6, val7, val8, val9, val10, val11,
+                        val12, val13, val14, val15, val16, val17,
+                        val18, val19, val20, val21, val22, val23;
 
-              args >> val0 >> val1 >> val2 >> val3 >> val4 >> val5
-                   >> val6 >> val7 >> val8 >> val9 >> val10 >> val11
-                   >> val12 >> val13 >> val14 >> val15 >> val16 >> val17
-                   >> val18 >> val19 >> val20 >> val21 >> val22 >> val23
-                   >> osc::EndMessage;
-              std::cout << "received '/deviceB' message with arguments: "
-                << val0 << " " << val1 << " " << val2 << " " << val3 << " "
-                << val4 << " " << val5 << " " << val6 << " " << val7 << " "
-                << val8 << " " << val9 << " " << val10 << " " << val11 << " "
-                << val12 << " " << val13 << " " << val14 << " " << val15 << " "
-                << val16 << " " << val17 << " " << val18 << " " << val19 << " "
-                << val20 << " " << val21 << " " << val22 << " " << val23 << " "
-                << "\n";
+                args >> val0 >> val1 >> val2 >> val3 >> val4 >> val5
+                        >> val6 >> val7 >> val8 >> val9 >> val10 >> val11
+                        >> val12 >> val13 >> val14 >> val15 >> val16 >> val17
+                        >> val18 >> val19 >> val20 >> val21 >> val22 >> val23
+                        >> osc::EndMessage;
+                std::cout << "received '/deviceB' message with arguments: "
+                          << val0 << " " << val1 << " " << val2 << " " << val3 << " "
+                          << val4 << " " << val5 << " " << val6 << " " << val7 << " "
+                          << val8 << " " << val9 << " " << val10 << " " << val11 << " "
+                          << val12 << " " << val13 << " " << val14 << " " << val15 << " "
+                          << val16 << " " << val17 << " " << val18 << " " << val19 << " "
+                          << val20 << " " << val21 << " " << val22 << " " << val23 << " "
+                          << "\n";
 
             } else if( strcmp( m.AddressPattern(), "/deviceC" ) == 0 ){
-              osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-              osc::int32 val0, val1, val2, val3, val4, val5,
-                         val6, val7, val8, val9, val10, val11,
-                         val12, val13, val14, val15, val16, val17,
-                         val18, val19, val20, val21, val22, val23;
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+                osc::int32 val0, val1, val2, val3, val4, val5,
+                        val6, val7, val8, val9, val10, val11,
+                        val12, val13, val14, val15, val16, val17,
+                        val18, val19, val20, val21, val22, val23;
 
-              args >> val0 >> val1 >> val2 >> val3 >> val4 >> val5
-                   >> val6 >> val7 >> val8 >> val9 >> val10 >> val11
-                   >> val12 >> val13 >> val14 >> val15 >> val16 >> val17
-                   >> val18 >> val19 >> val20 >> val21 >> val22 >> val23
-                   >> osc::EndMessage;
-              std::cout << "received '/deviceC' message with arguments: "
-                << val0 << " " << val1 << " " << val2 << " " << val3 << " "
-                << val4 << " " << val5 << " " << val6 << " " << val7 << " "
-                << val8 << " " << val9 << " " << val10 << " " << val11 << " "
-                << val12 << " " << val13 << " " << val14 << " " << val15 << " "
-                << val16 << " " << val17 << " " << val18 << " " << val19 << " "
-                << val20 << " " << val21 << " " << val22 << " " << val23 << " "
-                << "\n";
+                args >> val0 >> val1 >> val2 >> val3 >> val4 >> val5
+                        >> val6 >> val7 >> val8 >> val9 >> val10 >> val11
+                        >> val12 >> val13 >> val14 >> val15 >> val16 >> val17
+                        >> val18 >> val19 >> val20 >> val21 >> val22 >> val23
+                        >> osc::EndMessage;
+                std::cout << "received '/deviceC' message with arguments: "
+                          << val0 << " " << val1 << " " << val2 << " " << val3 << " "
+                          << val4 << " " << val5 << " " << val6 << " " << val7 << " "
+                          << val8 << " " << val9 << " " << val10 << " " << val11 << " "
+                          << val12 << " " << val13 << " " << val14 << " " << val15 << " "
+                          << val16 << " " << val17 << " " << val18 << " " << val19 << " "
+                          << val20 << " " << val21 << " " << val22 << " " << val23 << " "
+                          << "\n";
 
             }  else if( strcmp( m.AddressPattern(), "/deviceD" ) == 0 ){
-              osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-              osc::int32 val0, val1, val2, val3, val4, val5,
-                         val6, val7, val8, val9, val10, val11,
-                         val12, val13, val14, val15, val16, val17,
-                         val18, val19, val20, val21, val22, val23;
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+                osc::int32 val0, val1, val2, val3, val4, val5,
+                        val6, val7, val8, val9, val10, val11,
+                        val12, val13, val14, val15, val16, val17,
+                        val18, val19, val20, val21, val22, val23;
 
-              args >> val0 >> val1 >> val2 >> val3 >> val4 >> val5
-                   >> val6 >> val7 >> val8 >> val9 >> val10 >> val11
-                   >> val12 >> val13 >> val14 >> val15 >> val16 >> val17
-                   >> val18 >> val19 >> val20 >> val21 >> val22 >> val23
-                   >> osc::EndMessage;
-              std::cout << "received '/deviceD' message with arguments: "
-                << val0 << " " << val1 << " " << val2 << " " << val3 << " "
-                << val4 << " " << val5 << " " << val6 << " " << val7 << " "
-                << val8 << " " << val9 << " " << val10 << " " << val11 << " "
-                << val12 << " " << val13 << " " << val14 << " " << val15 << " "
-                << val16 << " " << val17 << " " << val18 << " " << val19 << " "
-                << val20 << " " << val21 << " " << val22 << " " << val23 << " "
-                << "\n";
-                }
+                args >> val0 >> val1 >> val2 >> val3 >> val4 >> val5
+                        >> val6 >> val7 >> val8 >> val9 >> val10 >> val11
+                        >> val12 >> val13 >> val14 >> val15 >> val16 >> val17
+                        >> val18 >> val19 >> val20 >> val21 >> val22 >> val23
+                        >> osc::EndMessage;
+                std::cout << "received '/deviceD' message with arguments: "
+                          << val0 << " " << val1 << " " << val2 << " " << val3 << " "
+                          << val4 << " " << val5 << " " << val6 << " " << val7 << " "
+                          << val8 << " " << val9 << " " << val10 << " " << val11 << " "
+                          << val12 << " " << val13 << " " << val14 << " " << val15 << " "
+                          << val16 << " " << val17 << " " << val18 << " " << val19 << " "
+                          << val20 << " " << val21 << " " << val22 << " " << val23 << " "
+                          << "\n";
+            }
         } catch( osc::Exception& e ) {
             // any parsing errors such as unexpected argument types, or
             // missing arguments get thrown as exceptions.
             std::cout << "error while parsing message: "
-                << m.AddressPattern() << ": " << e.what() << "\n";
+                      << m.AddressPattern() << ": " << e.what() << "\n";
         }
     }
 };
@@ -113,8 +113,8 @@ int main(int argc, char* argv[])
 {
     ExamplePacketListener listener;
     UdpListeningReceiveSocket s(
-            IpEndpointName( IpEndpointName::ANY_ADDRESS, PORT ),
-            &listener );
+                IpEndpointName( IpEndpointName::ANY_ADDRESS, PORT ),
+                &listener );
 
     std::cout << "press ctrl-c to end\n";
 
@@ -163,14 +163,14 @@ int gShutoff;
 
 
 void ctrlc(int aN) {
-	gShutoff = 1;
-	signal( SIGINT, NULL );
+    gShutoff = 1;
+    signal( SIGINT, NULL );
 }
 
 
 
 int main() {
-	gShutoff = 0;
+    gShutoff = 0;
   signal( SIGINT, ctrlc );
 
 
@@ -204,10 +204,10 @@ int main() {
  std::vector<int> values;
 
 
-	for(int i=0; i<1; i++){
-		Dacs1500rcp24 dio(i);
-		dioList.push_back(dio);
-	}
+    for(int i=0; i<1; i++){
+        Dacs1500rcp24 dio(i);
+        dioList.push_back(dio);
+    }
 
  // device open
  for(int i=0; i<1; i++) dioList[i].open();
@@ -215,36 +215,36 @@ int main() {
 
  // lamp on -> off
  for(int i=0; i<1; i++) {
-	 command = dioList[i].getDigitalOutPutCommand("800000");
+     command = dioList[i].getDigitalOutPutCommand("800000");
    dioList[i].sendCommand(command);
  }
  //dio.sendCommand(command);
  sleep(2);
  for(int i=0; i<1; i++) {
-	 command = dioList[i].getDigitalOutPutCommand("000000");
-	 dioList[i].sendCommand(command);
+     command = dioList[i].getDigitalOutPutCommand("000000");
+     dioList[i].sendCommand(command);
  }
  sleep(2);
 
  // pwm init
  for(int i=0; i<1; i++) {
-	 command = dioList[i].getPWMInitializeCommand(3,20000);
-	 dioList[i].sendCommand(command);
+     command = dioList[i].getPWMInitializeCommand(3,20000);
+     dioList[i].sendCommand(command);
  }
 
 
  // pwm init pos
  for(int i=0; i<1; i++) {
-	 for(int j=0; j<PWM_CHANNEL_NUM; j++) values.push_back(1450);
-	 command = dioList[i].getPWMPalseChangeCommand(values);
-	 dioList[i].sendCommand(command);
+     for(int j=0; j<PWM_CHANNEL_NUM; j++) values.push_back(1450);
+     command = dioList[i].getPWMPalseChangeCommand(values);
+     dioList[i].sendCommand(command);
  }
 
 
  // start
  for(int i=0; i<1; i++) {
-	 command = dioList[i].getPWMStartCommand();
-	 dioList[i].sendCommand(command);
+     command = dioList[i].getPWMStartCommand();
+     dioList[i].sendCommand(command);
  }
 
 
@@ -256,16 +256,16 @@ int main() {
    if(numrcv == -1) { status = close(recvSocket); break; }
    printf("received: %s\n", buffer);
 
-	 for(int i=0; i<1; i++) {
-		 command = buffer;
-		 dioList[i].sendCommand(command);
-	 }
+     for(int i=0; i<1; i++) {
+         command = buffer;
+         dioList[i].sendCommand(command);
+     }
 
  }
  // stop
  for(int i=0; i<1; i++) {
-	 command = dioList[i].getPWMStopCommand();
-	 dioList[i].sendCommand(command);
+     command = dioList[i].getPWMStopCommand();
+     dioList[i].sendCommand(command);
  }
 
 
@@ -295,56 +295,56 @@ int main() {
 
 int main(void)
 {
-	Dacs1500rcp24 dio(0);
-	std::string command;
-	std::vector<int> values;
+    Dacs1500rcp24 dio(0);
+    std::string command;
+    std::vector<int> values;
 
-	// device open
-	dio.open();
+    // device open
+    dio.open();
 
-	// lamp on -> off
-	command = dio.getDigitalOutPutCommand("800000");
-	dio.sendCommand(command);
-	sleep(2);
-	command = dio.getDigitalOutPutCommand("000000");
-	dio.sendCommand(command);
-	sleep(2);
+    // lamp on -> off
+    command = dio.getDigitalOutPutCommand("800000");
+    dio.sendCommand(command);
+    sleep(2);
+    command = dio.getDigitalOutPutCommand("000000");
+    dio.sendCommand(command);
+    sleep(2);
 
-	// pwm init
-	command = dio.getPWMInitializeCommand(3,20000);
-	dio.sendCommand(command);
+    // pwm init
+    command = dio.getPWMInitializeCommand(3,20000);
+    dio.sendCommand(command);
 
-	// pwm init pos
-	for(int i=0; i<PWM_CHANNEL_NUM; i++) values.push_back(1450);
-	command = dio.getPWMPalseChangeCommand(values);
-	dio.sendCommand(command);
+    // pwm init pos
+    for(int i=0; i<PWM_CHANNEL_NUM; i++) values.push_back(1450);
+    command = dio.getPWMPalseChangeCommand(values);
+    dio.sendCommand(command);
 
-	// start
-	command = dio.getPWMStartCommand();
-	dio.sendCommand(command);
+    // start
+    command = dio.getPWMStartCommand();
+    dio.sendCommand(command);
 
-	// changePWM
-	for (int i=0; i<10; i++) {
+    // changePWM
+    for (int i=0; i<10; i++) {
     sleep(1);
     if(i%2==0) {
-			for(int i=0; i<PWM_CHANNEL_NUM; i++) values[i] = 2400;
-			command = dio.getPWMPalseChangeCommand(values);
-			dio.sendCommand(command);
-		}
+            for(int i=0; i<PWM_CHANNEL_NUM; i++) values[i] = 2400;
+            command = dio.getPWMPalseChangeCommand(values);
+            dio.sendCommand(command);
+        }
     else {
-			for(int i=0; i<PWM_CHANNEL_NUM; i++) values[i] = 500;
-			command = dio.getPWMPalseChangeCommand(values);
-			dio.sendCommand(command);
-		}
-	}
+            for(int i=0; i<PWM_CHANNEL_NUM; i++) values[i] = 500;
+            command = dio.getPWMPalseChangeCommand(values);
+            dio.sendCommand(command);
+        }
+    }
 
-	// stop
-	command = dio.getPWMStopCommand();
-	dio.sendCommand(command);
+    // stop
+    command = dio.getPWMStopCommand();
+    dio.sendCommand(command);
 
-	// device close
-	dio.close();
+    // device close
+    dio.close();
 
-	return 0;
+    return 0;
 }
 */
