a7eee9898743b2520fd3b674e71d1bfbdb2742e5
chakio <kawasakiyousuke1@gmail.com>
2016-10-17 18:59:44 +0900

    test

--- src/oscListener.cpp
+++ src/oscListener.cpp
@@ -120,19 +120,22 @@ void raspiTest::ProcessMessage( const osc::ReceivedMessage& m,
 
         } else if( strcmp( m.AddressPattern(), "/deviceB" ) == 0 ){
             if(DIO_NUM >= 2) {
-                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-                args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
-                        >> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
-                        >> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
-                        >> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
-                        >> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
-                        >> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
-                        >> osc::EndMessage;
-                for(int j=0; j<PWM_CHANNEL_NUM; j++) values[j] = (int)oscValues[j];
-                command = dioList[1].getPWMPalseChangeCommand(values,0);
-                dioList[1].sendCommand(command);
-                dioList[1].receiveCommand(command.length());
-                //dioList[1].receiveCommand(24 * 9);
+				osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+				args >> oscValues[0] >> oscValues[1] >> oscValues[2] >> oscValues[3]
+					>> oscValues[4] >> oscValues[5] >> oscValues[6] >> oscValues[7]
+					>> oscValues[8] >> oscValues[9] >> oscValues[10] >> oscValues[11]
+					>> oscValues[12] >> oscValues[13] >> oscValues[14] >> oscValues[15]
+					>> oscValues[16] >> oscValues[17] >> oscValues[18] >> oscValues[19]
+					>> oscValues[20] >> oscValues[21] >> oscValues[22] >> oscValues[23]
+					>> osc::EndMessage;
+				for (int j = 0; j<PWM_CHANNEL_NUM*PWM_BRANCH_NUM; j++) values[j] = (int)oscValues[j];
+				command = dioList[0].getPWMPalseChangeCommand(values, 0);
+				dioList[0].sendCommand(command);
+				dioList[0].receiveCommand(command.length());
+				command = dioList[0].getPWMPalseChangeCommand(values, 1);
+				dioList[0].sendCommand(command);
+				dioList[0].receiveCommand(command.length());
+				//dioList[0].receiveCommand(24 * 9);
             }
 
         } else if( strcmp( m.AddressPattern(), "/deviceC" ) == 0 ){
